94e4da2634bc3824c57e6752602743a1f5c368a7
carte <carte8@naver.com>
2022-12-27 12:43:53 +0900

    Initial commit

---
+++ board_exam_project-master/src/main/java/com/study/board/entity/Board.java
@@ -0,0 +1,32 @@
+package com.study.board.entity;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Board  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column
+    private String title;
+    @Column
+    private String content;
+
+    @Column
+    @CreationTimestamp
+    private LocalDateTime createdDateTime;
+
+    @Column(columnDefinition = "integer default 0", nullable = false)
+    private int view;
+
+}
