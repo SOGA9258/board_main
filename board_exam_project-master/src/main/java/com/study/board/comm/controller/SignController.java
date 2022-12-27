a67edc50945e02cfde48112a3e10ebfd216d2dc3
pcg0902 <qkrcksrbs8@naver.com>
2022-12-27 13:49:24 +0900

    [로그인] 템플릿 구현

---
+++ board_exam_project-master/src/main/java/com/study/board/comm/controller/SignController.java
@@ -0,0 +1,21 @@
+package com.study.board.comm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/auth")
+@Controller
+public class SignController {
+
+    @GetMapping("/signin")
+    public String signin() {
+        return "auth/signin";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "auth/signup";
+    }
+
+}
